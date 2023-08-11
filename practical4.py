64508ae226927ac44ea42ddd4e46f9379cd7e124
Saloni <115524019+Salonisaroha@users.noreply.github.com>
2023-08-11 21:16:38 +0530

    Graphs

---
+++ practical4.py
@@ -0,0 +1,11 @@
+import statistics as st
+list = [12,34,56,78]
+Mean = st.mean(list)
+print(Mean)
+Median = st.median(list)
+print(Median)
+Mode = st.mode(list)
+print(Mode)
+val = list[(input("enter numbers"))]
+mean1 = st.mean1(val)
+print(val)
